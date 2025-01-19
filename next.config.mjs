cc8c87acc7479ba6ab3407fa818e46ca103d0e21
Radwan-iv <yone13435@gmail.com>
2025-01-19 19:26:10 +0200

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: false,
-    tsconfigPath: './tsconfig.json'
-  }
-};
-
-export default nextConfig;
